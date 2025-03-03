31c35ee66c0a0a47f23d685b5bde86d9b7eb8783
GabrielRossi01 <rossi17006@gmail.com>
2025-03-02 22:22:53 -0300

    Versionamento do projeto e criação de um array para armazenar tarefas como objetos e utilização do operador spread no arquivo script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Exercício 1
+
+// Array para armazenar as tarefas como objetos com id, titulo e concluída.
+let tarefas = [
+    {id: 1, titulo: 'Tarefa Inicial', concluida: false}
+];
+
+// Criando um novo array usando o operador spread
+tarefas = [...tarefas];
