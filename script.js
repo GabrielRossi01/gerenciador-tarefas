4d9bc31f0dc53f2e7383578f36b327a786ae7725
GabrielRossi01 <rossi17006@gmail.com>
2025-03-03 15:54:36 -0300

    Criando uma função para capturar o valor do input e criar uma nova tarefa

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// Exercício 1
-
-// Array para armazenar as tarefas como objetos com id, titulo e concluída.
+// Exercício 1: Criando um array para armazenar as tarefas e adicionando uma tarefa inicial 
 let tarefas = [
     {id: 1, titulo: 'Tarefa Inicial', concluida: false}
 ];
 
-// Criando um novo array usando o operador spread
-tarefas = [...tarefas];
+// Referências ao DOM
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const filterBtn = document.getElementById('filterBtn');
+const taskList = document.getElementById('taskList');
+
+// Exercício 2: Função para capturar o valor do input e criar uma nova tarefa
+function adicionarTarefa() {
+    if (taskInput.attributeStyleMap() === "") return alert('Digite uma tarefa válida!');
+}
